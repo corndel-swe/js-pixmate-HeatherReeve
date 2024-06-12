a689f71ce8a9983b12879827984da0bb779bda2e
heather Reeve <heather.reeve@scope.org.uk>
2024-06-12 11:11:27 +0100

    fixed with guide

--- exercises/1a.js
+++ exercises/1a.js
@@ -13,4 +13,17 @@
  */
 export function addEven(n) {
   // TODO
+  //loop is add even numbers 
+  //end of loop is when it hits n
+  let i = 1
+  let sum = 0 
+
+ while (i<= n) {
+if (i % 2==0){ 
+  sum +=i
+}
+i++ //i +i
+
+} 
+return sum
 }
