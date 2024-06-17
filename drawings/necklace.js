686b4d24b7b62c63ce147854f7274a1d5b10f30a
heather Reeve <heather.reeve@scope.org.uk>
2024-06-17 14:07:16 +0100

    completed

--- drawings/necklace.js
+++ drawings/necklace.js
@@ -3,18 +3,24 @@ import chalk from 'chalk'
 
 // start with an empty line
 let line = ''
-
+line += chalk.blue.italic.bgBlue('**p**')
 // use a loop to add stuff to the line
-for (let i = 0; i < 10; i++) {
+for (let i = 1; i <26; i++) {
   // some conditionals to produce a pattern
   if (i % 2 == 0) {
-    line += chalk.green('O')
+    line += chalk.yellow.underline('x')
   } else {
-    line += '-'.repeat(2)
+    line += ' - '.repeat(1)
+  } { 
+    line += chalk.greenBright('*'.repeat(2))
   }
 }
 
 // one more 'green bead' on the end!
-line += chalk.green('O')
+line += chalk.blue.bold.bgWhite('p**')
+
+
+
+
 
 console.log(line)
